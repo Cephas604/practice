08093ebc8b07311573fc7bd6cffa56ae8d40c343
Cephas604 <cephas.healthy604@gmail.com>
2022-08-23 22:26:35 +0900

    Add options

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,3 +1,5 @@
+require 'optparse'
+
 def to_hex(r,g,b)
   hex = '#'
   [r,g,b].sum('#') do |n|
@@ -6,6 +8,13 @@ def to_hex(r,g,b)
 end
 
 def to_ints(hex)
-  hex.scan(/\w\w/).map(&:hex)
+  hex.scan(/\w\w/).map(&:hex).join(',')
 end
 
+options = ARGV.getopts('h:i:', 'hex:', 'ints:')
+puts options
+r,g,b = options['h'].split(',').map(&:to_i) unless options['h'].nil?
+r,g,b = options['hex'].split(',').map(&:to_i) unless options['hex'].nil?
+puts to_hex(r,g,b) unless options['hex'].nil? && options['h'].nil?
+puts to_ints(options['i']) unless options['i'].nil?
+puts to_ints(options['ints']) unless options['ints'].nil?
