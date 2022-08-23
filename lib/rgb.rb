d74ee21db50dcff0ad2be4f15994303458dbe07c
Cephas604 <cephas.healthy604@gmail.com>
2022-08-23 22:49:16 +0900

    Delete .map

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -3,7 +3,7 @@ require 'optparse'
 def to_hex(r,g,b)
   hex = '#'
   [r,g,b].sum('#') do |n|
-  n.to_s(16).rjust(2,'0')
+  sprintf("%#x", n).delete("0x").rjust(2,'0')
   end
 end
 
@@ -13,8 +13,8 @@ end
 
 options = ARGV.getopts('h:i:', 'hex:', 'ints:')
 puts options
-r,g,b = options['h'].split(',').map(&:to_i) unless options['h'].nil?
-r,g,b = options['hex'].split(',').map(&:to_i) unless options['hex'].nil?
+r,g,b = options['h'].split(',') unless options['h'].nil?
+r,g,b = options['hex'].split(',') unless options['hex'].nil?
 puts to_hex(r,g,b) unless options['hex'].nil? && options['h'].nil?
 puts to_ints(options['i']) unless options['i'].nil?
 puts to_ints(options['ints']) unless options['ints'].nil?
